50e13f7995993856b3d990c542aaaaded6698b7e
Víctor Romário <victor.romario.seriado@gmail.com>
2020-04-03 21:04:01 -0300

    Adiciona lista de calendário
    A lista de calendário exibe as informações do array 'conteudo'. Foram criadas as classes no scss: a) head-list, para formatar o cabeçalho da lista; b)vertically-center, para colocar o conteúdo da lista no centro, verticalmente.

--- src/app/calendario/calendario.page.ts
+++ src/app/calendario/calendario.page.ts
@@ -9,6 +9,9 @@ import { NavController } from  '@ionic/angular';
 export class CalendarioPage implements OnInit {
 
   public funcionario: string;
+  public conteudo = [
+    {data:'01/02/2020',qtd:12},{data:'02/02/2020',qtd:8},{data:'03/02/2020',qtd:14},{data:'04/02/2020',qtd:5},{data:'05/02/2020',qtd:7},
+    {data:'06/02/2020',qtd:11},{data:'07/02/2020',qtd:9},{data:'08/02/2020',qtd:10},{data:'09/02/2020',qtd:10},{data:'10/02/2020',qtd:14}];
 
   constructor(private nav:NavController) {
     this.funcionario = "Fulano de Tal";
