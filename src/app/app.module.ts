2174ef89594f599978317e6d577adb07ebf9a181
Víctor Romário <victor.romario.seriado@gmail.com>
2020-04-04 03:47:17 -0300

    Coloca o estilo do app no padrão iOS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ReactiveFormsModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot({mode:'ios'}), AppRoutingModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
