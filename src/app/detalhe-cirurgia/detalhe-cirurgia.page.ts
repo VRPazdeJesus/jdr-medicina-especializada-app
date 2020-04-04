3f7d2ea3041705a51ab20c01f362a30666dc1a19
Víctor Romário <victor.romario.seriado@gmail.com>
2020-04-04 03:24:53 -0300

    Criação das variáveis dos detalhes da cirurgia

--- src/app/detalhe-cirurgia/detalhe-cirurgia.page.ts
+++ src/app/detalhe-cirurgia/detalhe-cirurgia.page.ts
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalheCirurgiaPage implements OnInit {
 
-  constructor() { }
+  public agendamento: string;
+  public data: string;
+  public hora: string;
+  public hospital: string;
+  public medico: string;
+  public convenio: string;
+  public paciente: string;
+  public cirurgia: string;
+
+  constructor() {
+    this.agendamento = 'Z11256';
+    this.data = '01/02/2020';
+    this.hora = '07:00';
+    this.hospital = 'Hospital Santa Izabel';
+    this.medico = 'Dr. Jack Tequila';
+    this.convenio = 'Bradesco';
+    this.paciente = 'Maria de Lourdes';
+    this.cirurgia = 'ATJ';
+  }
 
   ngOnInit() {
   }
