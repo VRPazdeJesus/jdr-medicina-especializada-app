941fc63b1747bb2ec7c73b60e8fd7ae186696d88
Víctor Romário <victor.romario.seriado@gmail.com>
2020-04-04 16:35:30 -0300

    Adiciona atributos que exibirão valores no frontend
    em 'lista-materiais'

--- src/app/lista-materiais/lista-materiais.page.ts
+++ src/app/lista-materiais/lista-materiais.page.ts
@@ -7,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaMateriaisPage implements OnInit {
 
-  constructor() { }
+  public agendamento: string;
+  public data: string;
+  public hospital: string;
+  public medico: string;
+  public convenio: string;
+  public paciente: string;
+  public cirurgia: string;
+
+  constructor() {
+    this.agendamento = 'Z00000';
+    this.data = '01/02/2020';
+    this.hospital = 'Lorem Ipsum Dolor';
+    this.medico = 'Lorem Ipsum Dolor';
+    this.convenio = 'Lorem Ipsum';
+    this.paciente = "Lorem Ipsum Dolor";
+    this.cirurgia = 'Lorem ipsum';
+  }
 
   ngOnInit() {
   }
