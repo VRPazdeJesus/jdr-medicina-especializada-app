8697732cf704ebc3c6f1d52d15b91048b3210d2d
Víctor Romário <victor.romario.seriado@gmail.com>
2020-04-04 22:04:34 -0300

    Adiciona os atributos no controller
    da página lista-materiais-instrumentador

--- src/app/lista-materiais-instrumentador/lista-materiais-instrumentador.page.ts
+++ src/app/lista-materiais-instrumentador/lista-materiais-instrumentador.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from  '@ionic/angular';
 
 @Component({
   selector: 'app-lista-materiais-instrumentador',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaMateriaisInstrumentadorPage implements OnInit {
 
-  constructor() { }
+  public agendamento: string;
+  public data: string;
+  public hospital: string;
+  public medico: string;
+  public convenio: string;
+  public paciente: string;
+  public cirurgia: string;
+  public conteudo = [
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1},
+    {produto:'AR-6410',descricao:'EQUIPO PARA BOMA',qtd:1}
+  ];
+
+  constructor(private nav:NavController) {
+    this.agendamento = 'Z00000';
+    this.data = '01/02/2020';
+    this.hospital = 'Lorem Ipsum Dolor';
+    this.medico = 'Lorem Ipsum Dolor';
+    this.convenio = 'Lorem Ipsum';
+    this.paciente = "Lorem Ipsum Dolor";
+    this.cirurgia = 'Lorem ipsum';
+  }
 
   ngOnInit() {
   }
 
+  public finalizar() {
+    this.nav.navigateForward('/detalhe-cirurgia');
+  }
 }
